dd8ec232e3f2b92791044d011e01c4d46727cb0f
michalpieszczyk <michalpieszczyk@gmail.com>
2020-06-13 16:34:31 +0200

    Tłumaczenie nazw do kodów w lodówce.
    Brakuje funkcji usuwania produktów z bazy.
    Brakuje wyświetlania szczegółów o produkcie (obecnie tylko nazwa)

--- app/src/main/java/com/example/qr/StanList.java
+++ app/src/main/java/com/example/qr/StanList.java
@@ -6,14 +6,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
 public class StanList extends ArrayAdapter<Member> {
 
+    DatabaseReference reference;
+    protected FirebaseDatabase database = FirebaseDatabase.getInstance();
     private Activity context;
     private List<Member> memberList;
 
@@ -29,11 +38,25 @@ public class StanList extends ArrayAdapter<Member> {
         LayoutInflater inflater = context.getLayoutInflater();
 
         View listViewItem = inflater.inflate(R.layout.list_layout, null, true);
-        TextView textViewNazwa = (TextView) listViewItem.findViewById(R.id.textViewNazwa);
+        final TextView textViewNazwa = (TextView) listViewItem.findViewById(R.id.textViewNazwa);
         TextView textViewData = (TextView) listViewItem.findViewById(R.id.textViewData);
 
         Member member = memberList.get(position);
-        textViewNazwa.setText(member.getKodKreskowy());
+        DatabaseReference myRef = database.getInstance().getReference("Produkty");
+        DatabaseReference myRef1 = myRef.child(member.getKodKreskowy());
+        DatabaseReference myRefnazwa2 = myRef1.child("nazwa");
+        myRefnazwa2.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                textViewNazwa.setText(dataSnapshot.getValue(String.class));
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            };
+        });
+       //textViewNazwa.setText(member.getKodKreskowy());
         textViewData.setText(member.getObecnaData());
 
 
